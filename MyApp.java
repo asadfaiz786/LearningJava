a41b46015b20596caf0dc22c4d3106ead75d1dea
asadfaiz786 <asadfaiz786@hotmail.com>
2022-05-21 23:49:38 -0400

    JavaLanguage

---
+++ MyApp.java
@@ -0,0 +1,9 @@
+public class MyApp {
+
+	public static void main(String[] args) {
+
+		System.out.println("This is my first App !!");
+		System.out.println("This my 2nd Line in Java");
+		System.out.println("This my 2nd Line in Java");
+	}
+}
---
+++ MyNew.java
@@ -0,0 +1,8 @@
+public class MyNew{
+
+public static void main(String[] args){
+
+        System.out.println("This is my first App !!");
+        System.out.println("This my 2nd Line in Java");                                 }
+
+}
